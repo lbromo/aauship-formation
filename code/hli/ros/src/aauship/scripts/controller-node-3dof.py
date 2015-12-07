48913a1cf88560011a460679d13a3d4146949ece
Lasse Bromose <lbromo12@student.aau.dk>
2015-12-07 15:08:47 +0100

    push

--- code/hli/ros/src/aauship/scripts/controller-node-3dof.py
+++ code/hli/ros/src/aauship/scripts/controller-node-3dof.py
@@ -21,20 +21,20 @@ class Controller(object):
 
         
 	def __init__(self):
-		self.i = 0
+                self.i = 0
                 self.first = True
-		self.lat = [0, 0]
-		self.long = [0, 0]
-		self.psi = [0, 0]
+                self.lat = [0, 0]
+                self.long = [0, 0]
+                self.psi = [0, 0]
                 
 		# Loading Matrices from MATLAB Model and Controller
-		matrices = sio.loadmat('../../../../../matlab/3_dof.mat')
-		# State Space Model Matrices
-		self.A = matrices['Ad']
-		self.B = matrices['Bd']
-		self.C = matrices['Cd']
-		# LQR Proportional Controller
-		self.LQR = matrices['LQR']
+                matrices = sio.loadmat('../../../../../matlab/3_dof.mat')
+                # State Space Model Matrices
+                self.A = matrices['Ad']
+                self.B = matrices['Bd']
+                self.C = matrices['Cd']
+                # LQR Proportional Controller
+                self.LQR = matrices['LQR']
 
 		rospy.init_node('Controller')
                 
@@ -44,10 +44,10 @@ class Controller(object):
                 print self.wp
 
 		data_sub = message_filters.Subscriber('kf_statesnew', KFStates)
-		gps_sub = message_filters.Subscriber('gps2', GPS)
+                gps_sub = message_filters.Subscriber('gps2', GPS)
                 self.pub = rospy.Publisher('lli_input', LLIinput, queue_size=1)
-		ts = message_filters.TimeSynchronizer([data_sub, gps_sub], 1)
-		ts.registerCallback(self.callback)
+                ts = message_filters.TimeSynchronizer([data_sub, gps_sub], 1)
+                ts.registerCallback(self.callback)
 
 
                 self.ref = np.matrix('%s; %s; 0; 0; 0; 0' % (self.wp.lat, self.wp.long))
@@ -65,22 +65,22 @@ class Controller(object):
                         return
 
 		self.psi[self.i % 2] = data.psi
-		self.lat[self.i % 2] = gps.latitude
-		self.long[self.i % 2] = gps.longitude
-		self.r = self.psi[self.i % 2] - self.psi[(self.i-1) % 2]
-		self.u = self.lat[self.i % 2] - self.lat[(self.i-1) % 2]
-		self.v = self.long[self.i % 2] - self.long[(self.i-1) % 2]
+                self.lat[self.i % 2] = gps.latitude
+                self.long[self.i % 2] = gps.longitude
+                self.r = self.psi[self.i % 2] - self.psi[(self.i-1) % 2]
+                self.u = self.lat[self.i % 2] - self.lat[(self.i-1) % 2]
+                self.v = self.long[self.i % 2] - self.long[(self.i-1) % 2]
 
-		self.x = np.matrix([[self.lat[self.i % 2]], [self.long[self.i % 2]], [self.psi[self.i % 2]], [self.u], [self.v], [self.r]])
+                self.x = np.matrix([[self.lat[self.i % 2]], [self.long[self.i % 2]], [self.psi[self.i % 2]], [self.u], [self.v], [self.r]])
                 self.send_controller_output()
-		self.i += 1
-		# print('#'*80)
-		# print("N:", self.lat[self.i % 2])
-		# print("E:", self.long[self.i % 2])
-		# print("PSI:", self.psi[self.i % 2])
-		# print("u:", self.u)
-		# print("v:", self.v)
-		# print("r:", self.r)
+                self.i += 1
+                # print('#'*80)
+                # print("N:", self.lat[self.i % 2])
+                # print("E:", self.long[self.i % 2])
+                # print("PSI:", self.psi[self.i % 2])
+                # print("u:", self.u)
+                # print("v:", self.v)
+                # print("r:", self.r)
 
         def send_controller_output(self):
                 m = [0, 0]
@@ -119,20 +119,20 @@ class Controller(object):
                         m[0] = (u[0] + 26.84) / 0.2746
                         if m[0] < 70:
                                 m[0] = 70
-                elif(u[0] < 0):
-                        m[0] = (u[0] + 7.318) / 0.1152
-                        if m[0] > -70:
-                                m[0] = -70
+                        elif(u[0] < 0):
+                                m[0] = (u[0] + 7.318) / 0.1152
+                                if m[0] > -70:
+                                        m[0] = -70
 
                 if(u[1] > 0):
                         m[1] = (u[1] + 26.84) / 0.2746
                         if m[1] < 70:
                                 m[1] = 70
-                elif(u[1] < 0):
-                        m[1] = (u[1] + 7.318) / 0.1152
-                        if m[1] > -70:
-                                m[1] = -70
-                
+                        elif(u[1] < 0):
+                                m[1] = (u[1] + 7.318) / 0.1152
+                                if m[1] > -70:
+                                        m[1] = -70
+                                        
                 
                 msg.DevID = 10
                 msg.MsgID = 3
@@ -144,50 +144,50 @@ class Controller(object):
                 self.pub.publish(msg)
                 
 
-	def plos_EBS(self,x_k,x_k_1,y_k,y_k_1,x,y,n,L):
-		# Constant slope between to points:
-		#
-		#                   yk+1 - yk       ylos - yk
-		# tan(alpha_k) =  ------------- = ------------- = constant
-		#                   xk+1 - xk       xlos - xk
-		# 
-		# Pythagoras Theorem: (Radius of search)
-		#
-		# [xlos - x(t)]^2 + [ylos - y(t)]^2 = (nL)^2
+    def plos_EBS(self,x_k,x_k_1,y_k,y_k_1,x,y,n,L):
+        # Constant slope between to points:
+        #
+        #                   yk+1 - yk       ylos - yk
+        # tan(alpha_k) =  ------------- = ------------- = constant
+        #                   xk+1 - xk       xlos - xk
+        # 
+        # Pythagoras Theorem: (Radius of search)
+        #
+        # [xlos - x(t)]^2 + [ylos - y(t)]^2 = (nL)^2
 
-		delta_x = x_k - x_k_1
-		delta_y = y_k - y_k_1
-		
+        delta_x = x_k - x_k_1
+                delta_y = y_k - y_k_1
+                
 
-		if delta_x == 0: 
-			x_los = x_k_1 #x_los = x_k;
-			if delta_y > 0: 
-				y_los = y + n*L
-			else:#delta_y < 0
-				y_los = y - n*L
+        if delta_x == 0: 
+                x_los = x_k_1 #x_los = x_k;
+                if delta_y > 0: 
+                        y_los = y + n*L
+                else:#delta_y < 0
+                        y_los = y - n*L
 
-		else: #delta_x~=0
-			d = delta_y/delta_x
-			e = x_k_1
-			f = y_k_1
-			g = -d*e + f
-			a = 1 + d**2
-			b = 2*(d*g - d*y - x)
-			c = x**2 + y**2 + g**2 - (n*L)**2 - 2*y*g
+        else: #delta_x~=0
+                d = delta_y/delta_x
+                e = x_k_1
+                f = y_k_1
+                g = -d*e + f
+                a = 1 + d**2
+                b = 2*(d*g - d*y - x)
+                c = x**2 + y**2 + g**2 - (n*L)**2 - 2*y*g
 
-			#ERROR HANDLING
-			if (b**2 - 4*a*c) < 0:
-				print 'ERROR'
-				print '[W]',x_k,y_k
-				print '[B]',x,y
+            #ERROR HANDLING
+            if (b**2 - 4*a*c) < 0:
+                    print 'ERROR'
+                    print '[W]',x_k,y_k
+                    print '[B]',x,y
 
-			if delta_x > 0:
-				x_los = (-b + math.sqrt(b**2 - 4*a*c) )/(2*a)
-			else:  #delta_x < 0
-				x_los = (-b - math.sqrt(b**2 - 4*a*c) )/(2*a);
-		
-			y_los = d*(x_los - e) + f;
-		
+                    if delta_x > 0:
+                            x_los = (-b + math.sqrt(b**2 - 4*a*c) )/(2*a)
+                    else:  #delta_x < 0
+                            x_los = (-b - math.sqrt(b**2 - 4*a*c) )/(2*a);
+                                
+                y_los = d*(x_los - e) + f;
+                            
                 # If we're going UP in North
                 if (x_k > x_k_1):
                         if x_los > x_k:
@@ -206,10 +206,10 @@ class Controller(object):
                         if y_los < y_k:
                                 y_los = y_k
 
-		return x_los,y_los;
+                return x_los,y_los;
 
 
 if __name__ == '__main__':
-	controller = Controller()
-	while not rospy.is_shutdown():
-		pass
+        controller = Controller()
+        while not rospy.is_shutdown():
+                pass
